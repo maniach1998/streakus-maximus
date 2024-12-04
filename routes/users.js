c96e689a40f535410a022b5db221a74b07438e7e
Manas Acharekar <manas.acharekar98@gmail.com>
2024-12-04 18:06:07 -0500

    display auth errors + minor schema refinements

--- routes/users.js
+++ routes/users.js
@@ -23,42 +23,45 @@ router
 			});
 		} catch (err) {
 			if (err instanceof z.ZodError) {
-				return res.status(400).json({
-					success: false,
-					error: err.errors,
-				});
+				return res
+					.status(400)
+					.render("auth/signup", { title: "Sign Up", errors: err.errors });
 			} else {
-				return res.status(err.cause || 500).json({
-					success: false,
-					error: err.message || "Internal server error",
+				return res.status(err.cause || 500).render("auth/signup", {
+					title: "Sign Up",
+					rootError: err.message || "Internal server error",
 				});
 			}
 		}
 	});
 
-router.route("/login").post(async (req, res) => {
-	try {
-		const validatedData = loginSchema(req.body);
+router
+	.route("/login")
+	.get(async (req, res) => {
+		return res.render("auth/login", { title: "Log in" });
+	})
+	.post(async (req, res) => {
+		try {
+			const validatedData = loginSchema.parse(req.body);
 
-		const user = await loginUser(validatedData);
+			const user = await loginUser(validatedData);
 
-		return res.json({
-			success: true,
-			user,
-		});
-	} catch (err) {
-		if (err instanceof z.ZodError) {
-			return res.status(400).json({
-				success: false,
-				error: err.errors,
+			return res.json({
+				success: true,
+				user,
 			});
-		} else {
-			return res.status(err.cause || 500).json({
-				success: false,
-				error: err.message || "Internal server error",
-			});
+		} catch (err) {
+			if (err instanceof z.ZodError) {
+				return res
+					.status(400)
+					.render("auth/login", { title: "Log in", errors: err.errors });
+			} else {
+				return res.status(err.cause || 500).render("auth/login", {
+					title: "Log in",
+					rootError: err.message || "Internal server error",
+				});
+			}
 		}
-	}
-});
+	});
 
 export default router;
--- schemas/users.js
+++ schemas/users.js
@@ -2,15 +2,38 @@ import { z } from "zod";
 import { ObjectId } from "mongodb";
 
 export const userSchema = z.object({
-	firstName: z.string().min(2),
-	lastName: z.string().min(2),
+	firstName: z
+		.string()
+		.refine((name) => name.trim().length > 2, {
+			message: "First name cannot be less than 2 characters long!",
+		})
+		.refine((name) => /[a-zA-z]/.test(name), {
+			message: "First name can only be alphabets!",
+		})
+		.transform((name) => name.trim()),
+	lastName: z
+		.string()
+		.refine((name) => name.trim().length > 2, {
+			message: "First name cannot be less than 2 characters long!",
+		})
+		.transform((name) => name.trim()),
 	email: z.string().email(),
-	password: z.string().min(8),
+	password: z
+		.string()
+		.refine((password) => password.trim().length > 8, {
+			message: "Password should be more than 8 characters long!",
+		})
+		.transform((password) => password.trim()),
 });
 
 export const loginSchema = z.object({
 	email: z.string().email(),
-	password: z.string().min(8),
+	password: z
+		.string()
+		.refine((password) => password.trim().length > 8, {
+			message: "Password should be more than 8 characters long!",
+		})
+		.transform((password) => password.trim()),
 });
 
 export const userIdSchema = z.object({
