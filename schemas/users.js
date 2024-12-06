8f5bb3bce0154cb965119f4f499cd37e94c2ffc0
Manas Acharekar <manas.acharekar98@gmail.com>
2024-12-06 00:07:15 -0500

    refine user schema validation

--- schemas/users.js
+++ schemas/users.js
@@ -4,41 +4,45 @@ import { ObjectId } from "mongodb";
 export const userSchema = z.object({
 	firstName: z
 		.string()
-		.refine((name) => name.trim().length > 2, {
+		.transform((name) => name.trim())
+		.refine((name) => name.length > 2, {
 			message: "First name cannot be less than 2 characters long!",
 		})
-		.refine((name) => /[a-zA-z]/.test(name), {
-			message: "First name can only be alphabets!",
-		})
-		.transform((name) => name.trim()),
+		.refine((name) => /^[a-zA-z]+$/.test(name), {
+			message: "First name can only contain alphabets!",
+		}),
 	lastName: z
 		.string()
-		.refine((name) => name.trim().length > 2, {
-			message: "First name cannot be less than 2 characters long!",
+		.transform((name) => name.trim())
+		.refine((name) => name.length > 2, {
+			message: "Last name cannot be less than 2 characters long!",
 		})
-		.transform((name) => name.trim()),
+		.refine((name) => /^[a-zA-z]+$/.test(name), {
+			message: "Last name can only contain alphabets!",
+		}),
 	email: z.string().email(),
 	password: z
 		.string()
-		.refine((password) => password.trim().length > 8, {
+		.transform((password) => password.trim())
+		.refine((password) => password.length > 8, {
 			message: "Password should be more than 8 characters long!",
-		})
-		.transform((password) => password.trim()),
+		}),
 });
 
 export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z
 		.string()
-		.refine((password) => password.trim().length > 8, {
+		.transform((password) => password.trim())
+		.refine((password) => password.length > 8, {
 			message: "Password should be more than 8 characters long!",
-		})
-		.transform((password) => password.trim()),
+		}),
 });
 
 export const userIdSchema = z.object({
 	_id: z
 		.string()
+		.transform((id) => id.trim())
 		.refine((id) => ObjectId.isValid(id), { message: "Not a valid ObjectId!" }),
 });
 
