75b2bd16bbab411a1a090df039ba5f5f85320de5
Manas Acharekar <manas.acharekar98@gmail.com>
2024-12-05 21:43:13 -0500

    use Inter font for app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./views/**/*.handlebars", "./public/**/*.js"],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
